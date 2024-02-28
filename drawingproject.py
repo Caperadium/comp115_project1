cdcb160dccbca6699c636c3ffbac9eb2e797df24
Caperadium <39166344+Caperadium@users.noreply.github.com>
2024-02-27 21:13:06 -0800

    Update drawingproject.py

--- drawingproject.py
+++ drawingproject.py
@@ -1,10 +1,24 @@
 import turtle
+import random
+
+random.seed()
 
 pen = turtle.Turtle()
 screen = turtle.Screen()
 
+def draw_leg(): #draws one single brown rectangle
+    pen.fillcolor('brown')
+    pen.begin_fill()
+    pen.right(90)
+    for _ in range(2):
+        pen.forward(150)
+        pen.left(90)
+        pen.forward(20)
+        pen.left(90)
+    pen.end_fill()
+    
 
-def draw_water(length, width): #draws water of certain size
+def draw_tank(length, width): #draws fish tank of certain size
     pen.fillcolor('blue') #draws rectangle of water
     pen.begin_fill()
     for _ in range(2):
@@ -13,8 +27,13 @@ def draw_water(length, width): #draws water of certain size
         pen.forward(width)
         pen.left(90)
     pen.end_fill()
+    draw_leg() #calls leg draw function to draw leg
+    pen.left(90) 
+    pen.forward(length-20)
+    draw_leg()
+    pen.left(90)
 
-def draw_fish(x, y): #draws fish at certain location
+def draw_fish(x, y): #draws fish at certain location. It is really not effecient at all but i'm a terrible artist and really wanted to draw a fish
     pen.up()
     pen.goto(x,y)
     pen.down()
@@ -42,11 +61,33 @@ def draw_fish(x, y): #draws fish at certain location
     pen.down()
     pen.circle(5)
     pen.end_fill()
-     
-   
-    
 
-draw_water(400, 200)
-draw_fish(20, 50)
+def draw_sun(): #calls random function to calculate random number of sun rays, then turtle goes to specified location to draw sun
+    num_rays = random.randint(4,16)
+    pen.up()
+    pen.goto(170,170)
+    pen.down()
+    pen.fillcolor('yellow')
+    pen.begin_fill()
+    pen.circle(30)
+    pen.end_fill()
+    angle = 360/num_rays
+    pen.up()
+    pen.left(90)
+    pen.forward(30)
+
+    for i in range(num_rays): #iterates over perviously calculated random number of rays and draws rays
+        pen.up()
+        pen.forward(30)
+        pen.down()
+        pen.forward(10)
+        pen.up()
+        pen.back(40)
+        pen.right(angle)
+
+draw_tank(200, 100) #draws tank with legs
+draw_fish(20, 50) #draws fish in tank
+draw_sun() #draws sun
+
 
 screen.exitonclick()
